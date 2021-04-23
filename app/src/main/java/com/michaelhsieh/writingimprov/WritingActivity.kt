56b778fbe3b35cb3166738ddbf4c890c688e3fe5
Michael Hsieh <mdhsieh8@gmail.com>
2021-04-22 21:47:12 -0700

    Tested on API 17 tablet. Random image does not load.
    Tried to fix error 2 different ways, but neither worked.
    
    First tried allowing cleartext traffic.
    Second tried to create OkHttp client that accepted all certificates.

--- app/src/main/java/com/michaelhsieh/writingimprov/WritingActivity.kt
+++ app/src/main/java/com/michaelhsieh/writingimprov/WritingActivity.kt
@@ -1,18 +1,30 @@
 package com.michaelhsieh.writingimprov
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Callback
+import com.squareup.picasso.OkHttp3Downloader
 import com.squareup.picasso.Picasso
-import java.lang.Exception
+import okhttp3.OkHttpClient
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
 
-private const val RAND_IMAGE_URL:String = "https://source.unsplash.com/random/800x800"
+/**
+ * Load random image and start countdown timer if loaded successfully
+ *
+ * References:
+ * https://codinginflow.com/tutorials/android/countdowntimer/part-1-countdown-timer
+ * https://github.com/square/picasso/issues/1896
+ */
+// private const val RAND_IMAGE_URL:String = "https://source.unsplash.com/random/800x800"
+private const val RAND_IMAGE_URL:String = "https://images.unsplash.com/photo-1618053448492-2b629c2c912c?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=800&ixlib=rb-1.2.1&q=80&w=800"
+// private const val RAND_IMAGE_URL:String = "http://source.unsplash.com/random/800x800"
 private const val TAG:String = "WritingActivity"
 private const val KEY_MILLIS_LEFT:String = "millisLeft"
 private const val KEY_END_TIME:String = "endTime"
@@ -47,6 +59,9 @@ class WritingActivity : AppCompatActivity() {
         // Load the random image
         val image = findViewById<ImageView>(R.id.iv_image)
         Picasso.get().load(RAND_IMAGE_URL)
+        // Allow HTTP requests to fix Picasso 504 timeout error on older devices,
+        // example API 17 tablet
+        // getPicassoUnsafeCertificate(this).load(RAND_IMAGE_URL)
                         .error(R.drawable.ic_error_outline_72)
                         .into(image, object : Callback {
                             override fun onSuccess() {
@@ -100,6 +115,46 @@ class WritingActivity : AppCompatActivity() {
         val timeLeftFormatted:String = String.format("%02d:%02d", minutes, seconds)
 
         timerText.text = timeLeftFormatted
+    }
+
+    private fun getUnsafeOkHttpClient(): OkHttpClient {
+        // Create a trust manager that does not validate certificate chains
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(
+                chain: Array<out X509Certificate>?,
+                authType: String?
+            ) {
+            }
+
+            override fun checkServerTrusted(
+                chain: Array<out X509Certificate>?,
+                authType: String?
+            ) {
+            }
+
+            override fun getAcceptedIssuers() = arrayOf<X509Certificate>()
+        })
+
+        // Install the all-trusting trust manager
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+        // Create an ssl socket factory with our all-trusting manager
+        val sslSocketFactory = sslContext.socketFactory
+
+        return OkHttpClient.Builder()
+            .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+            .hostnameVerifier(HostnameVerifier { _, _ -> true })
+            .build()
+    }
+
+    // add client to Picasso
+    private fun getPicassoUnsafeCertificate(context: Context): Picasso {
+        val client = getUnsafeOkHttpClient()
+        Log.d(TAG, "got unsafe builder")
+        val picasso = Picasso.Builder(context).downloader(OkHttp3Downloader(client)).build()
+        picasso.isLoggingEnabled = true
+        Log.d(TAG, "got new picasso")
+        return picasso
     }
 
     // Keep timer running after configuration change, example on rotation

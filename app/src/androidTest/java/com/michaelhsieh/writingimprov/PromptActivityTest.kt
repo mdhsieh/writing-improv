3ad98294dc47477ea335412dd6dcbc458695efd3
Michael Hsieh <mdhsieh8@gmail.com>
2021-05-08 16:31:20 -0700

    refactor: Move check visibility of image, time text, and prompt text into one test.
    Also created function to try to get text of a TextView.
    
    Tested on API 28 emulator.

--- app/src/androidTest/java/com/michaelhsieh/writingimprov/PromptActivityTest.kt
+++ app/src/androidTest/java/com/michaelhsieh/writingimprov/PromptActivityTest.kt
@@ -1,10 +1,17 @@
 package com.michaelhsieh.writingimprov
 
+import android.view.View
+import android.widget.TextView
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
 
@@ -21,24 +28,19 @@ class PromptActivityTest {
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
+    /**
+     * Check question mark image, random time text, and random prompt text are visible.
+     */
     @Test
-    fun test_visibility_image() {
+    fun test_visibility_image_timeText_promptText() {
 
         Espresso.onView(ViewMatchers.withId(R.id.iv_image))
             .perform(ViewActions.scrollTo())
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-    }
-
-    @Test
-    fun test_visibility_timeText() {
 
         Espresso.onView(ViewMatchers.withId(R.id.tv_time))
             .perform(ViewActions.scrollTo())
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-    }
-
-    @Test
-    fun test_visibility_promptText() {
 
         Espresso.onView(ViewMatchers.withId(R.id.tv_prompt))
             .perform(ViewActions.scrollTo())
@@ -60,29 +62,28 @@ class PromptActivityTest {
             .check(ViewAssertions.matches(ViewMatchers.withText(R.string.go)))
     }
 
-    // go to PromptActivity when practice button clicked
-//    @Test
-//    fun test_navPromptActivity() {
-//
-//        Espresso.onView(ViewMatchers.withId(R.id.btn_practice)).perform(ViewActions.click())
-//
-//        Espresso.onView(ViewMatchers.withId(R.id.scroll_view_prompt))
-//            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-//    }
+    /**
+     * Get the text of a TextView.
+     * Used to check if a TextView value is the same after rotation.
+     */
+    fun getText(matcher: ViewInteraction): String {
+        var text = String()
+        matcher.perform(object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isAssignableFrom(TextView::class.java)
+            }
 
-    // go to PromptActivity, then go back to HomeFragment
-//    @Test
-//    fun test_backPress_toHomeFragment() {
-//
-//        Espresso.onView(ViewMatchers.withId(R.id.btn_practice)).perform(ViewActions.click())
-//
-//        Espresso.onView(ViewMatchers.withId(R.id.scroll_view_prompt))
-//            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-//
-//        Espresso.pressBack()
-//
-//        Espresso.onView(ViewMatchers.withId(R.id.home))
-//            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-//
-//    }
+            override fun getDescription(): String {
+                return "Text of the view"
+            }
+
+            override fun perform(uiController: UiController, view: View) {
+                val tv = view as TextView
+                text = tv.text.toString()
+            }
+        })
+
+        return text
+    }
+
 }

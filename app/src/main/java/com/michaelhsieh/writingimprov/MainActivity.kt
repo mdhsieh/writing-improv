c94f090c8b2a5aa6937df833d711aa01086da987
Michael Hsieh <mdhsieh8@gmail.com>
2021-05-11 01:02:36 -0700

    feat: Create toolbar.
    Toolbar is to have a back button on top of screen.
    
    Runs on API 28 emulator.
    
    There is bug of app crashing after rotating twice and press back.

--- app/src/main/java/com/michaelhsieh/writingimprov/MainActivity.kt
+++ app/src/main/java/com/michaelhsieh/writingimprov/MainActivity.kt
@@ -3,6 +3,11 @@ package com.michaelhsieh.writingimprov
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException
 import com.google.android.gms.common.GooglePlayServicesRepairableException
 import com.google.android.gms.common.GooglePlayServicesUtil
@@ -20,13 +25,27 @@ import timber.log.Timber
  * https://stackoverflow.com/questions/29916962/javax-net-ssl-sslhandshakeexception-javax-net-ssl-sslprotocolexception-ssl-han
  */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+
         Timber.plant(Timber.DebugTree())
 
         updateAndroidSecurityProvider(this)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     /**

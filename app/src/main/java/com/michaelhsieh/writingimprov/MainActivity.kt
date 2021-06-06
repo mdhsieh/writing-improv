51b4b3f2240e8f60c6cd6a34a65fdcead9bfdab4
Michael Hsieh <mdhsieh8@gmail.com>
2021-06-06 15:43:06 -0700

    feat: Add settings screen.
    Create settings fragment, layout, add to nav graph, and add options menu.
    Menu item ID has same ID as settings fragment in order to connect to nav graph.
    
    Runs on API 28 emulator.

--- app/src/main/java/com/michaelhsieh/writingimprov/MainActivity.kt
+++ app/src/main/java/com/michaelhsieh/writingimprov/MainActivity.kt
@@ -3,6 +3,8 @@ package com.michaelhsieh.writingimprov
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -10,6 +12,7 @@ import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException
 import com.google.android.gms.common.GooglePlayServicesRepairableException
@@ -58,6 +61,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController)
     }
 
     /**
---
+++ app/src/main/java/com/michaelhsieh/writingimprov/SettingsFragment.kt
@@ -0,0 +1,6 @@
+package com.michaelhsieh.writingimprov
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+}

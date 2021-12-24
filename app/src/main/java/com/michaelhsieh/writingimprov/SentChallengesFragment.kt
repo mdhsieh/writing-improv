97f50c481fa3c3e25855d1e30ebc0f849b274927
Michael Hsieh <mdhsieh8@gmail.com>
2021-12-24 13:44:57 -0800

    feat: Display and add placeholder WritingItem if challenge not completed by other user yet. Display and add WritingItems which are completed by other users.

--- app/src/main/java/com/michaelhsieh/writingimprov/SentChallengesFragment.kt
+++ app/src/main/java/com/michaelhsieh/writingimprov/SentChallengesFragment.kt
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.michaelhsieh.writingimprov.HomeFragment.Companion.COLLECTION_CHALLENGES
+import com.michaelhsieh.writingimprov.HomeFragment.Companion.COLLECTION_WRITING
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import es.dmoral.toasty.Toasty
@@ -26,9 +27,9 @@ import kotlin.collections.ArrayList
  *
  */
 
-class SentChallengesFragment:Fragment(R.layout.fragment_sent_challenges), ChallengesAdapter.ItemClickListener {
+class SentChallengesFragment:Fragment(R.layout.fragment_sent_challenges), MyWritingAdapter.ItemClickListener {
 
-    private lateinit var adapter: ChallengesAdapter
+    private lateinit var adapter: MyWritingAdapter
 
     var db = FirebaseFirestore.getInstance()
 
@@ -36,13 +37,13 @@ class SentChallengesFragment:Fragment(R.layout.fragment_sent_challenges), Challe
         super.onViewCreated(view, savedInstanceState)
 
         // data to populate the RecyclerView with
-        val challengeItems: ArrayList<ChallengeItem> = ArrayList()
+        val writingItems: ArrayList<WritingItem> = ArrayList()
 
         val context = this.requireContext()
         // set up the RecyclerView
         val recyclerView: RecyclerView = view.findViewById(R.id.rv_sent_challenges)
         recyclerView.layoutManager = LinearLayoutManager(context)
-        adapter = ChallengesAdapter(context, challengeItems)
+        adapter = MyWritingAdapter(context, writingItems)
         adapter.setClickListener(this)
         recyclerView.adapter = adapter
 
@@ -54,7 +55,7 @@ class SentChallengesFragment:Fragment(R.layout.fragment_sent_challenges), Challe
 
         val progressBar = view.findViewById<ProgressBar>(R.id.pb_loading_sent_challenges)
         // Get reference to TextView
-        val emptyChallengesText = view.findViewById<TextView>(R.id.tv_sent_challenges_empty)
+        val emptySentChallengesText = view.findViewById<TextView>(R.id.tv_sent_challenges_empty)
 
         // Get existing users from Firestore
         val collection = db.collection(HomeFragment.COLLECTION_USERS)
@@ -76,47 +77,7 @@ class SentChallengesFragment:Fragment(R.layout.fragment_sent_challenges), Challe
                         }
                     }
 
-                    // For each user ID, get all challenge collection items which
-                    // have current user email as senderId
-                    for ((i, otherId) in otherUserIds.withIndex()) {
-                        collection
-                            .document(otherId)
-                            .collection(COLLECTION_CHALLENGES)
-                            .whereEqualTo("senderId", email)
-                            .get()
-                            .addOnSuccessListener {
-
-                                // Convert the whole Query Snapshot to a list
-                                // of objects directly
-                                val items: List<ChallengeItem> =
-                                    it.toObjects(ChallengeItem::class.java)
-
-                                // Set to list
-                                // challengeItems.clear()
-                                challengeItems.addAll(items)
-//                                Timber.d("onSuccess: %s", challengeItems)
-                                Toasty.info(this.requireContext(), "found some items: " + items.size.toShort(), Toast.LENGTH_SHORT).show()
-
-                                // if is last collection items list, then done and set pbar and text visibility
-                                if (i == otherUserIds.size - 1) {
-                                    // Reload RecyclerView
-                                    adapter.notifyDataSetChanged()
-                                    // Hide progress bar
-                                    progressBar.visibility = View.GONE
-                                    // Show or hide no writing text
-                                    setEmptyTextVisibility(challengeItems.size, emptyChallengesText)
-                                }
-
-
-                            }
-                            .addOnFailureListener {
-                                Timber.e(it)
-                                Toasty.error(this.requireContext(), R.string.error_loading_challenges, Toast.LENGTH_LONG).show()
-
-                                // Hide progress bar
-                                progressBar.visibility = View.GONE
-                            }
-                    }
+                    findAllChallengesWithSenderEmail(email, otherUserIds, writingItems, progressBar, emptySentChallengesText)
 
                 }
                 .addOnFailureListener {
@@ -160,5 +121,151 @@ class SentChallengesFragment:Fragment(R.layout.fragment_sent_challenges), Challe
             return user.email
         }
         return null
+    }
+
+    /**
+     * Get all challenges with senderId matching current user email.
+     * Then for each challenge's ID find all writing from other users with that ID
+     * @param senderId Current user email
+     * @param otherIds Every users' email excluding current user, as Strings in an ArrayList
+     * @param writings ArrayList to append other users' writings from the challenges
+     */
+    private fun findAllChallengesWithSenderEmail(senderId:String, otherIds:ArrayList<String>, writings:ArrayList<WritingItem>, pBar:ProgressBar, emptyTextView: TextView) {
+        val challenges = arrayListOf<ChallengeItem>()
+
+        // Get existing users from Firestore
+        val collection = db.collection(HomeFragment.COLLECTION_USERS)
+
+        // For each user ID, get all challenge collection items which
+        // have current user email as senderId
+        for ((i, otherId) in otherIds.withIndex()) {
+            collection
+                .document(otherId)
+                .collection(COLLECTION_CHALLENGES)
+                .whereEqualTo("senderId", senderId)
+                .get()
+                .addOnSuccessListener {
+
+                    // Convert the whole Query Snapshot to a list
+                    // of objects directly
+                    val items: List<ChallengeItem> =
+                        it.toObjects(ChallengeItem::class.java)
+
+                    // Set to list
+                    challenges.addAll(items)
+                    Toasty.info(this.requireContext(), "found challenges: " + items.size.toShort(), Toast.LENGTH_SHORT).show()
+
+                    // if reached last collection items list, then done
+                    // getting all challenges
+                    if (i == otherIds.size - 1) {
+
+                        appendAllWritingWithChallengeID(otherIds, challenges, writings, pBar, emptyTextView)
+
+                        /*
+                        // Set progress bar and text visibility
+                        // Reload RecyclerView
+                        adapter.notifyDataSetChanged()
+                        // Hide progress bar
+                        progressBar.visibility = View.GONE
+                        // Show or hide no writing text
+                        setEmptyTextVisibility(challengeItems.size, emptyChallengesText)
+                         */
+                    }
+
+
+                }
+                .addOnFailureListener {
+                    Timber.e(it)
+                    Toasty.error(this.requireContext(), R.string.error_loading_challenges, Toast.LENGTH_LONG).show()
+
+                    // Hide progress bar
+                    // progressBar.visibility = View.GONE
+                    pBar.visibility = View.GONE
+                }
+        }
+    }
+
+    /**
+     * Append to an ArrayList all writings which were submitted for
+     * the challenge with given ID.
+     *
+     * A challenge may have more than one writing.
+     * @param otherUserIds: Emails of all users excluding the current user
+     * @param challengeItems: Challenges the current user has sent to other users
+     * @param writingItems: ArrayList to append to
+     */
+    private fun appendAllWritingWithChallengeID(otherUserIds: ArrayList<String>, challengeItems:ArrayList<ChallengeItem>, writingItems:ArrayList<WritingItem>, progressBar: ProgressBar, emptySentChallengesText:TextView) {
+        // Get existing users from Firestore
+        val collection = db.collection(HomeFragment.COLLECTION_USERS)
+
+        // Need indexing to know when finished looping through all challenges and writing collections
+        for ((k,challenge) in challengeItems.withIndex()) {
+            var isWritingFound = false
+            for ((m,otherId) in otherUserIds.withIndex()) {
+                collection
+                    .document(otherId)
+                    .collection(COLLECTION_WRITING)
+                    .whereEqualTo("challengeId", challenge.id)
+                    .get()
+                    .addOnSuccessListener {
+                        // Convert the whole Query Snapshot to a list
+                        // of objects directly
+                        val items: List<WritingItem> =
+                            it.toObjects(WritingItem::class.java)
+
+                        writingItems.addAll(items)
+
+                        // if there is at least one writing collection with some writings of this challenge,
+                        // set to true
+                        if (items.isNotEmpty()) {
+                            isWritingFound = true
+                        }
+
+                        Toasty.info(this.requireContext(), "found writings: " + writingItems.size + " with challenge ID " + challenge.id, Toast.LENGTH_SHORT).show()
+
+                        // If reached last writing list and still no writings were found from this challenge,
+                        // then append a WritingItem with an empty prompt to display to user and
+                        // indicate this challenge has not been completed yet
+                        if (m == otherUserIds.size - 1 && !isWritingFound) {
+                            Toasty.info(this.requireContext(), "incomplete challenge: " + challenge.id, Toast.LENGTH_SHORT).show()
+                            writingItems.add(
+                                WritingItem(UUID.randomUUID().toString(),
+                                    name = "Challenge sent to " + challenge.receiverId,
+                                    prompt = challenge.prompt,
+                                    time = challenge.time,
+                                    url = challenge.url,
+                                    thumbUrl = challenge.thumbUrl,
+                                    writing = "",
+                                    challengeId = challenge.id
+                                )
+                            )
+                        }
+
+                        // if reached last challenge ID and last writing list of that ID, then done
+                        // getting all sent challenge writings to review.
+                        // If there are no challenges or writings with those challenge IDs, then
+                        // also done
+                        if (challengeItems.size == 0 || writingItems.size == 0 ||
+                            k == challengeItems.size - 1 && m == otherUserIds.size - 1) {
+                            // Toasty.info(this.requireContext(), "found writings: " + writingItems.size, Toast.LENGTH_LONG).show()
+
+                            // Set progress bar and text visibility
+                            // Reload RecyclerView
+                            adapter.notifyDataSetChanged()
+                            // Hide progress bar
+                            progressBar.visibility = View.GONE
+                            // Show or hide no writing text
+                            setEmptyTextVisibility(writingItems.size, emptySentChallengesText)
+                        }
+
+                    }
+                    .addOnFailureListener {
+                        Timber.e(it)
+                        Toasty.error(this.requireContext(), R.string.error_loading_my_writing, Toast.LENGTH_LONG).show()
+                        // Hide progress bar
+                        // progressBar.visibility = View.GONE
+                    }
+            }
+        }
     }
 }

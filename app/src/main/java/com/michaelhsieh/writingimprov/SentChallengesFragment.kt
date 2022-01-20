5267e26f61f67a67ee751d5a9a48bb8f4e6ea425
Michael Hsieh <mdhsieh8@gmail.com>
2022-01-20 15:06:43 -0800

    feat: Delete challenges user has sent other users.
    Deleted from Firestore by getting challenge ID from writing item in sent challenges RecyclerView.

--- app/src/main/java/com/michaelhsieh/writingimprov/SentChallengesFragment.kt
+++ app/src/main/java/com/michaelhsieh/writingimprov/SentChallengesFragment.kt
@@ -1,5 +1,6 @@
 package com.michaelhsieh.writingimprov
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.*
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -25,6 +27,8 @@ import kotlin.collections.ArrayList
  * Display clickable list of submitted and not submitted writings
  * from challenges the user has sent to other users.
  *
+ * Note list of WritingItem for each challenge, not the challenges themselves.
+ * Got the challenge by challengeId field in Firestore writing document
  */
 
 class SentChallengesFragment:Fragment(R.layout.fragment_sent_challenges), SentChallengesAdapter.ItemClickListener {
@@ -39,6 +43,30 @@ class SentChallengesFragment:Fragment(R.layout.fragment_sent_challenges), SentCh
         // data to populate the RecyclerView with
         val writingItems: ArrayList<WritingItem> = ArrayList()
 
+        // Swipe to delete
+        val itemTouchHelperCallback =
+            object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val email = getEmail()
+                    if (email != null) {
+                        val writingFromChallengeToDelete = adapter.getItem(viewHolder.adapterPosition)
+                        createDeleteConfirmationDialog(writingFromChallengeToDelete, viewHolder, writingItems, email)
+                    } else {
+                        Timber.d("Email is null")
+                        Toasty.error(this@SentChallengesFragment.requireContext(), R.string.error_email_deleted_writing).show()
+                    }
+                }
+
+            }
+
         val context = this.requireContext()
         // set up the RecyclerView
         val recyclerView: RecyclerView = view.findViewById(R.id.rv_sent_challenges)
@@ -52,6 +80,10 @@ class SentChallengesFragment:Fragment(R.layout.fragment_sent_challenges), SentCh
             (recyclerView.layoutManager as LinearLayoutManager).orientation
         )
         recyclerView.addItemDecoration(dividerItemDecoration)
+
+        // swipe to delete
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
 
         val progressBar = view.findViewById<ProgressBar>(R.id.pb_loading_sent_challenges)
         // Get reference to TextView
@@ -121,6 +153,131 @@ class SentChallengesFragment:Fragment(R.layout.fragment_sent_challenges), SentCh
             return user.email
         }
         return null
+    }
+
+    /**
+     * Display a dialog warning user they are deleting a challenge.
+     * If user selects yes, remove item from list, update RecyclerView,
+     * then remove from Firestore,
+     * then display success or failure Toast.
+     * If no, then don't remove
+     * @param writingItem: User completed writing or non-existent writing from the challenge to delete
+     * @param itemViewHolder: RecyclerView ViewHolder
+     * @param items: List of writing items for each challenge sent to other users
+     * @param userId: Firestore document user ID which is email
+     */
+    private fun createDeleteConfirmationDialog(writingItem:WritingItem, itemViewHolder: RecyclerView.ViewHolder, items: ArrayList<WritingItem>, userId:String) {
+        val builder = AlertDialog.Builder(this@SentChallengesFragment.requireContext())
+        builder.setMessage(getString(R.string.delete_confirmation, writingItem.name, writingItem.prompt))
+            .setCancelable(false)
+            .setPositiveButton(R.string.yes) { dialog, id ->
+                // deleteChallengeOfWriting(writingItem, itemViewHolder, items, userId)
+                getAllOtherUsersAndDeleteChallenges(userId, writingItem, itemViewHolder, items)
+            }
+            .setNegativeButton(R.string.no) { dialog, id ->
+                // Dismiss the dialog
+                dialog.dismiss()
+
+                // Make swiped out view animate back to original position
+                adapter.notifyItemChanged(itemViewHolder.adapterPosition)
+            }
+        val alert = builder.create()
+        alert.show()
+    }
+
+    /**
+     * Find challenge which the writing item is associated with,
+     * update RecyclerView,
+     * remove challenge from Firestore,
+     * then display success or failure Toast.
+     *
+     * @param writingFromChallengeToDelete: writing to get challenge to delete
+     * @param viewHolder: RecyclerView ViewHolder
+     * @param writingItems: List of writing items
+     * @param otherUserEmail: Firestore document user ID which is the challenge receiver's email.
+     * The challenge is stored in their collection
+     */
+    private fun deleteChallengeOfWriting(
+        writingFromChallengeToDelete:WritingItem, viewHolder:RecyclerView.ViewHolder,
+        writingItems: java.util.ArrayList<WritingItem>, otherUserEmail:String) {
+
+        // Start delete from Firestore
+        val collection = db.collection(HomeFragment.COLLECTION_USERS)
+            .document(otherUserEmail)
+            .collection(HomeFragment.COLLECTION_CHALLENGES)
+
+        val challengeId = writingFromChallengeToDelete.challengeId
+        collection.whereEqualTo("id", challengeId)
+            .get()
+            .addOnSuccessListener {
+                for (doc in it.documents) {
+                    // Get document ID which is different from writing ID field
+                    // and use that to delete document instead
+                    val docRefId = doc.id
+                    collection.document(docRefId).delete()
+                        .addOnSuccessListener {
+
+                            writingItems.removeAt(viewHolder.adapterPosition)
+                            adapter.notifyItemRemoved(viewHolder.adapterPosition)
+
+                            Toasty.info(
+                                this@SentChallengesFragment.requireContext(),
+                                getString(R.string.deleted_challenge, writingFromChallengeToDelete.name),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        .addOnFailureListener {
+                            Timber.d(it)
+                            Toasty.info(
+                                this@SentChallengesFragment.requireContext(),
+                                getString(R.string.error_deleted_challenge, writingFromChallengeToDelete.name),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            // Make swiped out view animate back to original position
+                            adapter.notifyItemChanged(viewHolder.adapterPosition)
+                        }
+                }
+            }
+            .addOnFailureListener {
+                Timber.d("Error getting docs where equal to")
+                Timber.d(it)
+
+                Toasty.info(
+                    this@SentChallengesFragment.requireContext(),
+                    getString(R.string.error_deleted_challenge, writingFromChallengeToDelete.name),
+                    Toast.LENGTH_SHORT
+                ).show()
+                // Make swiped out view animate back to original position
+                adapter.notifyItemChanged(viewHolder.adapterPosition)
+            }
+    }
+
+    /**
+     * Get the emails of all users, excluding the current user.
+     * To delete challenges sent to other users
+     * @param email The current user's own email
+     */
+    private fun getAllOtherUsersAndDeleteChallenges(email:String,
+                                                    writingFromChallengeToDelete:WritingItem, viewHolder:RecyclerView.ViewHolder, writingItems: java.util.ArrayList<WritingItem>) {
+        // Get existing users from Firestore
+        val collection = db.collection(HomeFragment.COLLECTION_USERS)
+        collection
+            .get()
+            .addOnSuccessListener {
+                val otherUserIds = arrayListOf<String>()
+                // Get all user IDs which aren't user himself or herself
+                for (doc in it.documents) {
+                    if (doc.id != email) {
+                        otherUserIds.add(doc.id)
+                        deleteChallengeOfWriting(writingFromChallengeToDelete, viewHolder, writingItems, doc.id)
+                    }
+                }
+                Timber.d("other user IDs: %s", otherUserIds.toString())
+            }
+            .addOnFailureListener {
+                Timber.e(it)
+                Toasty.error(this.requireContext(), R.string.error_email_deleted_writing, Toast.LENGTH_LONG).show()
+            }
     }
 
     /**

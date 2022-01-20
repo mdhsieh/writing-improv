b230e9bcdd047bcd3490f3f33431143fd4e7aeab
Michael Hsieh <mdhsieh8@gmail.com>
2022-01-20 13:37:05 -0800

    feat: Remove my challenge item from RecyclerView and Firestore.
    Manually tested with 4.2.2 tablet and 7.0 phone.
    Sometimes not all items will appear in challenges list, and sometimes a duplicate item will appear.

--- app/src/main/java/com/michaelhsieh/writingimprov/ChallengesFragment.kt
+++ app/src/main/java/com/michaelhsieh/writingimprov/ChallengesFragment.kt
@@ -1,5 +1,6 @@
 package com.michaelhsieh.writingimprov
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.*
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -36,6 +38,30 @@ class ChallengesFragment:Fragment(R.layout.fragment_challenges), ChallengesAdapt
         // data to populate the RecyclerView with
         val challengeItems: ArrayList<ChallengeItem> = ArrayList()
 
+        // Swipe to delete
+        val itemTouchHelperCallback =
+            object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val email = getEmail()
+                    if (email != null) {
+                        val challengeToDelete = adapter.getItem(viewHolder.adapterPosition)
+                        createDeleteConfirmationDialog(challengeToDelete, viewHolder, challengeItems, email)
+                    } else {
+                        Timber.d("Email is null")
+                        Toasty.error(this@ChallengesFragment.requireContext(), R.string.error_email_deleted_writing).show()
+                    }
+                }
+
+            }
+
         val context = this.requireContext()
         // set up the RecyclerView
         val recyclerView: RecyclerView = view.findViewById(R.id.rv_challenges)
@@ -49,6 +75,10 @@ class ChallengesFragment:Fragment(R.layout.fragment_challenges), ChallengesAdapt
             (recyclerView.layoutManager as LinearLayoutManager).orientation
         )
         recyclerView.addItemDecoration(dividerItemDecoration)
+
+        // swipe to delete
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
 
         val progressBar = view.findViewById<ProgressBar>(R.id.pb_loading_challenges)
         // Get reference to TextView
@@ -119,6 +149,97 @@ class ChallengesFragment:Fragment(R.layout.fragment_challenges), ChallengesAdapt
         val item = adapter.getItem(position)
         val action = ChallengesFragmentDirections.actionChallengesFragmentToWritingFragment(item.time.toInt(), item.prompt, item.url, item.thumbUrl, true, item.name, item.id)
         findNavController().navigate(action)
+    }
+
+    /**
+     * Display a dialog warning user they are deleting a challenge.
+     * If user selects yes, remove item from list, update RecyclerView,
+     * then remove from Firestore,
+     * then display success or failure Toast.
+     * If no, then don't remove
+     * @param challengeItem: Challenge to delete
+     * @param itemViewHolder: RecyclerView ViewHolder
+     * @param items: List of challenge items
+     * @param userId: Firestore document user ID which is email
+     */
+    private fun createDeleteConfirmationDialog(challengeItem:ChallengeItem, itemViewHolder: RecyclerView.ViewHolder, items: ArrayList<ChallengeItem>, userId:String) {
+        val builder = AlertDialog.Builder(this@ChallengesFragment.requireContext())
+        builder.setMessage(getString(R.string.delete_confirmation, challengeItem.name, challengeItem.prompt))
+            .setCancelable(false)
+            .setPositiveButton(R.string.yes) { dialog, id ->
+                deleteChallenge(challengeItem, itemViewHolder, items, userId)
+            }
+            .setNegativeButton(R.string.no) { dialog, id ->
+                // Dismiss the dialog
+                dialog.dismiss()
+
+                // Make swiped out view animate back to original position
+                adapter.notifyItemChanged(itemViewHolder.adapterPosition)
+            }
+        val alert = builder.create()
+        alert.show()
+    }
+
+    /**
+     * Remove selected writing item from list, update RecyclerView,
+     * remove from Firestore,
+     * then display success or failure Toast.
+     *
+     * @param challengeToDelete: Challenge to delete
+     * @param viewHolder: RecyclerView ViewHolder
+     * @param challengeItems: List of challenge items
+     * @param email: Firestore document user ID which is email
+     */
+    private fun deleteChallenge(
+        challengeToDelete:ChallengeItem, viewHolder:RecyclerView.ViewHolder,
+        challengeItems:ArrayList<ChallengeItem>, email:String) {
+        challengeItems.removeAt(viewHolder.adapterPosition)
+        adapter.notifyItemRemoved(viewHolder.adapterPosition)
+
+        // Start delete from Firestore
+        val collection = db.collection(HomeFragment.COLLECTION_USERS)
+            .document(email)
+            .collection(HomeFragment.COLLECTION_CHALLENGES)
+
+        collection.whereEqualTo("id", challengeToDelete.id)
+            .get()
+            .addOnSuccessListener {
+                for (doc in it.documents) {
+                    // Get document ID which is different from writing ID field
+                    // and use that to delete document instead
+                    val docRefId = doc.id
+                    collection.document(docRefId).delete()
+                        .addOnSuccessListener {
+                            Toasty.info(
+                                this@ChallengesFragment.requireContext(),
+                                getString(R.string.deleted_challenge, challengeToDelete.name),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        .addOnFailureListener {
+                            Timber.d(it)
+                            Toasty.info(
+                                this@ChallengesFragment.requireContext(),
+                                getString(R.string.error_deleted_challenge, challengeToDelete.name),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            // Make swiped out view animate back to original position
+                            adapter.notifyItemChanged(viewHolder.adapterPosition)
+                        }
+                }
+            }
+            .addOnFailureListener {
+                Timber.d("Error getting docs where equal to")
+                Timber.d(it)
+
+                Toasty.info(
+                    this@ChallengesFragment.requireContext(),
+                    getString(R.string.error_deleted_challenge, challengeToDelete.name),
+                    Toast.LENGTH_SHORT
+                ).show()
+                // Make swiped out view animate back to original position
+                adapter.notifyItemChanged(viewHolder.adapterPosition)
+            }
     }
 
     /**
